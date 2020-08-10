d1b5b38533f5dad1f491321aa8d14d15ee7cdb51
Connor Roberts <46317504+connor-roberts@users.noreply.github.com>
2020-08-09 20:29:37 -0400

    Create grocery-functions.js
    "x" remove button does not work yet

---
+++ grocery-app/grocery-functions.js
@@ -0,0 +1,66 @@
+const getSavedItems = function() {
+    const itemsJSON = localStorage.getItem("items")
+    if (itemsJSON !== null) {
+        return JSON.parse(itemsJSON)
+    } else {
+        return []
+    }
+}
+
+const saveItems = function (items) {
+    localStorage.setItem("items", JSON.stringify(items))
+}
+
+const removeItem = function (id) {
+    const index = items.findIndex(function(item) {
+        return item.id === id
+    })
+    items.splice(index, 1)
+    console.log(items)
+}
+
+const generateItemList = function(items) {
+    items.forEach(function(item) {
+        // Div
+        const itemWrapper = document.createElement("div")
+        // Checkbox
+        const itemCheckbox = document.createElement("input")
+        itemCheckbox.setAttribute("type", "checkbox")
+        itemCheckbox.checked = item.purchased
+        itemWrapper.appendChild(itemCheckbox)
+        itemCheckbox.addEventListener("change", function(e) {
+            item.purchased = !item.purchased
+            saveItems(items)
+            renderItems(items, filters)
+        })
+        // Text
+        const newItem = document.createElement("a")
+        newItem.setAttribute("href", "#")
+        newItem.textContent = item.name
+        itemWrapper.appendChild(newItem)
+        // Button
+        const removeButton = document.createElement("button")
+        removeButton.textContent = "x"
+        itemWrapper.appendChild(removeButton)
+        removeButton.addEventListener("click", function(e) {
+            removeItem(item.id)
+
+        })
+
+        // Append wrapped item
+        document.querySelector("#items").appendChild(itemWrapper)
+    })
+}
+
+const renderItems = function(items, filters) {
+    // Filter
+    const filteredItems = items.filter(function(item) {
+        const searchMatch = item.name.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideMatch = !filters.hidePurchased || !item.purchased
+        return searchMatch && hideMatch
+    })
+    // Clear div
+   document.querySelector("#items").innerHTML = ""
+    // Add items
+    generateItemList(filteredItems)
+}
